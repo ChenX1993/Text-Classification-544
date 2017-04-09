ace093f221beeadf8c73155f8fa0f1721e7047fa
ChenX1993 <xc454981894@gmail.com>
2017-04-09 16:47:57 -0700

    123

--- svm/SVM3.py
+++ svm/SVM3.py
@@ -88,7 +88,6 @@ def selectAlpha_j(svm, alpha_i, error_i):
 # the inner loop for optimizing alpha i and alpha j  
 def innerLoop(svm, alpha_i):  
     error_i = calcError(svm, alpha_i)  
-  
     ### check and pick up the alpha who violates the KKT condition  
     ## satisfy KKT condition  
     # 1) yi*f(i) >= 1 and alpha == 0 (outside the boundary)  
@@ -99,8 +98,7 @@ def innerLoop(svm, alpha_i):
     # 1) if y[i]*E_i < 0, so yi*f(i) < 1, if alpha < C, violate!(alpha = C will be correct)   
     # 2) if y[i]*E_i > 0, so yi*f(i) > 1, if alpha > 0, violate!(alpha = 0 will be correct)  
     # 3) if y[i]*E_i = 0, so yi*f(i) = 1, it is on the boundary, needless optimized  
-    if (svm.train_y[alpha_i] * error_i < -svm.toler) and (svm.alphas[alpha_i] < svm.C) or (svm.train_y[alpha_i] * error_i > svm.toler) and (svm.alphas[alpha_i] > 0):  
-  
+    if ((svm.train_y[alpha_i] * error_i < -svm.toler) and (svm.alphas[alpha_i] < svm.C)) or ((svm.train_y[alpha_i] * error_i > svm.toler) and (svm.alphas[alpha_i] > 0)):
         # step 1: select alpha j  
         alpha_j, error_j = selectAlpha_j(svm, alpha_i, error_i)  
         alpha_i_old = svm.alphas[alpha_i].copy()  
@@ -113,15 +111,16 @@ def innerLoop(svm, alpha_i):
         else:  
             L = max(0, svm.alphas[alpha_j] + svm.alphas[alpha_i] - svm.C)  
             H = min(svm.C, svm.alphas[alpha_j] + svm.alphas[alpha_i])  
-        if L == H:
-            return 0
+        # if L == H:
+        #     return 0
         # step 3: calculate eta (the similarity of sample i and j)  
         eta = 2.0 * svm.kernelMat[alpha_i, alpha_j] - svm.kernelMat[alpha_i, alpha_i] - svm.kernelMat[alpha_j, alpha_j]  
         if eta >= 0:  
             return 0  
   
         # step 4: update alpha j  
-        svm.alphas[alpha_j] -= svm.train_y[alpha_j] * (error_i - error_j) / eta  
+        svm.alphas[alpha_j] -= svm.train_y[alpha_j] * (error_i - error_j) / eta
+        #print svm.alphas[alpha_j]  
   
         # step 5: clip alpha j  
         if svm.alphas[alpha_j] > H:  
@@ -176,10 +175,10 @@ def trainSVM(train_x, train_y, C, toler, maxIter, kernelOption = ('rbf', 1.0)):
     
     while (iterCount < maxIter) and ((alphaPairsChanged > 0) or entireSet):  
         alphaPairsChanged = 0
-        printout = nonzero(svm.alphas.A > 0)[0]
-        for i in printout:
-            print svm.alphas[i, 0]
-        print printout
+        # printout = nonzero(svm.alphas.A > 0)[0]
+        # for i in printout:
+        #     print svm.alphas[i, 0]
+        #print printout
         # update alphas over all training examples  
         if entireSet:  
             for i in xrange(svm.numSamples):  
